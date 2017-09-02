73f4e4f6f6b9fe9b2d3c3900de78465c9dcbebd5
David Perez <dave.a.perez@gmail.com>
2017-09-02 01:34:32 -0700

    Add project files.

---
+++ Raw Data Communicator/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Raw_Data_Communicator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textToSend = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textToSend
+            // 
+            this.textToSend.Location = new System.Drawing.Point(12, 12);
+            this.textToSend.Multiline = true;
+            this.textToSend.Name = "textToSend";
+            this.textToSend.Size = new System.Drawing.Size(260, 118);
+            this.textToSend.TabIndex = 0;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Enabled = false;
+            this.sendButton.Location = new System.Drawing.Point(197, 136);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 1;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(12, 136);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.textToSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textToSend;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Button connectButton;
+    }
+}
+
---
+++ Raw Data Communicator/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Raw_Data_Communicator
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client = new TcpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void connectButton_Click(object sender, EventArgs e)
+        {
+            await client.ConnectAsync("localhost", 9000);
+            connectButton.Enabled = false;
+            sendButton.Enabled = true;
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(textToSend.Text);
+            client.GetStream().WriteAsync(data, 0, textToSend.Text.Length);
+            textToSend.Text = string.Empty;
+        }
+    }
+}
